373ec046f494119eaafb856b4da24e2f85c0202a
Shubham Roy Choudhury (Industrial SH20045650 Engineering Services (ISH20045650ES)) <SH20045650@wipro.com>
2019-08-22 13:27:46 +0530

    some updates

--- src/main/java/com/tourofheroes/repository/HeroRepository.java
+++ src/main/java/com/tourofheroes/repository/HeroRepository.java
@@ -3,7 +3,6 @@ package com.tourofheroes.repository;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Repository;
